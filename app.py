8d35a685260597343241baf03e84ff31df753474
Hamza <muhammadhamzahabib0@gmail.com>
2024-10-19 13:24:41 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,143 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+import timm
+import torchvision.transforms as transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class SimpleCardClassifer(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifer, self).__init__()
+        # Where we define all the parts of the model
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+        # Make a classifier
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+    
+    def forward(self, x):
+        # Connect these parts and return the output
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+model = SimpleCardClassifer()
+model.load_state_dict(torch.load('card_classifier_model.pt', map_location=torch.device('cpu'), weights_only=True))
+
+# Load and preprocess the image
+def preprocess_image(image_path, transform):
+    image = Image.open(image_path).convert("RGB")
+    return image, transform(image).unsqueeze(0)
+
+# Predict using the model
+def predict(model, image_tensor, device):
+    model.eval()
+    with torch.no_grad():
+        image_tensor = image_tensor.to(device)
+        outputs = model(image_tensor)
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    return probabilities.cpu().numpy().flatten()
+
+# Visualization
+def visualize_predictions(original_image, probabilities, class_names):
+    fig, axarr = plt.subplots(1, 2, figsize=(14, 7))
+    
+    # Display image
+    axarr[0].imshow(original_image)
+    axarr[0].axis("off")
+    
+    # Display predictions
+    axarr[1].barh(class_names, probabilities)
+    axarr[1].set_xlabel("Probability")
+    axarr[1].set_title("Class Predictions")
+    axarr[1].set_xlim(0, 1)
+
+    plt.tight_layout()
+    plt.show()
+
+# APP 
+st.title(":orange[Cards Classifier]")
+
+input_image = st.file_uploader("Upload the Image of the Card to predict...", type=['jpg', 'jpeg', 'png'])
+
+classes = ['ace of clubs',
+ 'ace of diamonds',
+ 'ace of hearts',
+ 'ace of spades',
+ 'eight of clubs',
+ 'eight of diamonds',
+ 'eight of hearts',
+ 'eight of spades',
+ 'five of clubs',
+ 'five of diamonds',
+ 'five of hearts',
+ 'five of spades',
+ 'four of clubs',
+ 'four of diamonds',
+ 'four of hearts',
+ 'four of spades',
+ 'jack of clubs',
+ 'jack of diamonds',
+ 'jack of hearts',
+ 'jack of spades',
+ 'joker',
+ 'king of clubs',
+ 'king of diamonds',
+ 'king of hearts',
+ 'king of spades',
+ 'nine of clubs',
+ 'nine of diamonds',
+ 'nine of hearts',
+ 'nine of spades',
+ 'queen of clubs',
+ 'queen of diamonds',
+ 'queen of hearts',
+ 'queen of spades',
+ 'seven of clubs',
+ 'seven of diamonds',
+ 'seven of hearts',
+ 'seven of spades',
+ 'six of clubs',
+ 'six of diamonds',
+ 'six of hearts',
+ 'six of spades',
+ 'ten of clubs',
+ 'ten of diamonds',
+ 'ten of hearts',
+ 'ten of spades',
+ 'three of clubs',
+ 'three of diamonds',
+ 'three of hearts',
+ 'three of spades',
+ 'two of clubs',
+ 'two of diamonds',
+ 'two of hearts',
+ 'two of spades']
+
+
+
+if st.button("Predict"):
+    if input_image == None:
+        st.write("Please upload the image...")
+    else:
+        transform = transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor()
+        ])
+        org_img, img_tensor = preprocess_image(input_image, transform)
+        probabilities = predict(model, img_tensor, device)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header(f"The Card in the Image is :orange[{classes[np.argmax(probabilities)]}].")
+        with col2:
+            st.image(org_img)
